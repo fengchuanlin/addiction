67ef0b7f02bd9121f8963184b6efae258cbc1a7e
冯传林 <1837733729@qq.com>
2020-07-16 18:20:49 +0800

    index 1.2
    add 轮播、背景图、导航

--- js/grjl.js
+++ js/grjl.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-	$("#min_img").click(function() {
-		var _this = $(this); //将当前的min_img元素作为_this传入函数
-		imgShow("#outerdiv", "#innerdiv", "#max_img", _this);
-	});
-});
+var mySwiper = new Swiper('.swiper-container', {
+	autoplay: true,//可选选项，自动滑动
+	loop : true,
+})
 
+//如果你初始化时没有定义Swiper实例，后面也可以通过Swiper的HTML元素来获取该实例
+new Swiper('.swiper-container')
+var mySwiper = document.querySelector('.swiper-container').swiper
+mySwiper.slideNext();
 
 
