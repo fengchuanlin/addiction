0df768201dc2c6a499d83086452073fb7e62c44c
fengchuanlin <1837733729@qq.com>
2020-09-25 20:33:00 +0800

    1.222
    更改内容

--- js/grjl.js
+++ js/grjl.js
@@ -1,3 +1,4 @@
+// 获得奖项
 var mySwiper = new Swiper('.swiper-container', {
 	autoplay: true,//可选选项，自动滑动
 	loop : true,
@@ -8,4 +9,21 @@ new Swiper('.swiper-container')
 var mySwiper = document.querySelector('.swiper-container').swiper
 mySwiper.slideNext();
 
+
+// 个人作品
+$(function(){
+	
+	$(".zuop_ul li").click(function(){
+		// 初始化
+		$(".zuop_ul li").attr("class","fre");
+		// 点击改变当前样式
+		
+		a=$(this).attr("id").substring(3,4);
+		
+		$(".cone").css("display","none");
+		$("#div_"+a).css("display","flex");
+	})
+	
+	
+})
 
